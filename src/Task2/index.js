a41fa6e5eb9527d6d0cbea72dfcabeb72fe883d6
Hayk Malkhasyan <hayk_malkhasyan@epam.com>
2022-12-16 23:20:48 +0100

    fix(task2): fix file read/write stream lyne by line

--- src/Task2/index.js
+++ src/Task2/index.js
@@ -1,9 +1,31 @@
 import path from 'path';
+import fs from 'fs';
 import { csv } from 'csvtojson';
 
 const csvFilePath = path.resolve(__dirname, 'csv/example.csv');
+const loggerPath = path.resolve(__dirname, 'csv/log.txt');
+
 csv()
   .fromFile(csvFilePath)
-  .then((jsonObj) => {
-    console.log(jsonObj);
-  });
+  .subscribe(
+    (lineData) => {
+      return new Promise((resolve, reject) => {
+        const logger = fs.createWriteStream(loggerPath, {
+          flags: 'a'
+        });
+
+        logger.on('error', function (err) {
+          console.log('ERROR:' + err);
+          reject();
+        });
+
+        resolve(logger.write(JSON.stringify(lineData) + '\n'));
+      });
+    },
+    (err) => {
+      console.error(err);
+    },
+    () => {
+      console.log('DONE !');
+    }
+  );
